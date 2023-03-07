5bd704e40e7d10dd794c3f5cba02a2b9480adfe2
Andrei Marc <42412680+andreimarc90@users.noreply.github.com>
2023-03-07 16:55:15 +0200

    Create homeworkSession1.cs

---
+++ homeworkSession1/homeworkSession1.cs
@@ -0,0 +1,33 @@
+int a, b,c;
+Console.WriteLine("Give me a value for number \"a\": ");
+a = int.Parse(Console.ReadLine());    
+Console.WriteLine("Give me a value for number \"a\": ");
+b = int.Parse(Console.ReadLine());
+
+
+//INT OPERATIONS:
+
+//Addition
+
+c = a + b;
+Console.WriteLine(a + " + " + b + " = "  + c);
+
+//Subtraction
+
+c = a - b;
+Console.WriteLine(a + " - " + b + " = " + c);
+
+//Multiplication
+
+c = a * b;
+Console.WriteLine(a + " * " + b + " = " + c);
+
+//Division
+
+c = a / b;
+Console.WriteLine(a + " / " + b + " = " + c);
+
+//Modulo
+
+c = a % b;
+Console.WriteLine(a + " % " + b + " = " + c);
